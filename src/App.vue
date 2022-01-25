c20d79cb4aab4c768fefcc95c6fad4d5cbc12e71
HyunJe Ryu <dighek456@naver.com>
2022-01-25 17:13:47 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app id="app">
+    <vtabs />
+    <div>{{테스트용}}</div>
+  </v-app>
+  
+</template>
+
+
+<script>
+import vtabs from './components/tabs.vue';
+
+export default {
+  name: 'App',
+  components: {
+    vtabs
+  },
+  methods: {
+
+  },
+  data: () => ({
+    테스트용 : 0,
+  }),
+};
+</script>
+<style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+.a {color:#fff;}
+</style>
---
+++ src/components/tabs.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-sheet elevation="6">
+    <v-tabs background-color="#5F54F0" dark next-icon="mdi-arrow-right-bold-box-outline" prev-icon="mdi-arrow-left-bold-box-outline" show-arrows>
+      <v-tabs-slider color="white"></v-tabs-slider>
+      <v-tab v-for="i in 6" :key="i" :href="'#tab-' + i">
+        설명 {{ i }}
+      </v-tab>
+    </v-tabs>
+  </v-sheet>
+</template>
